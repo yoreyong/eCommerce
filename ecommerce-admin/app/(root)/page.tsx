e18660e4cefda979196ac156749ec9077293b40a
YORE <yoreyongwang@gmail.com>
2023-07-31 16:14:41 -0500

    [admin]feat: Add dialog component

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
   return (
-    <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
+    <div>
+      <Modal
+        title="Test"
+        description="Test Desc"
+        isOpen
+        onClose={()=>{}}
+      >
+        Children
+      </Modal>
     </div>
-
   )
 }
 
