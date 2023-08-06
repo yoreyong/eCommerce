186dc9aa175938807d5db76f12a5d9347b98e4d8
YORE <yoreyongwang@gmail.com>
2023-08-05 21:41:09 -0500

    [admin]feat:不提交, 内容如上

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,39 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function DashBoardLayout({
+  children,
+  params
+} : {
+  children: React.ReactNode
+  params: { storeId: string}
+}
+){
+  const { userId } = auth();
+  if(!userId) {
+    redirect('/sign-in');
+  }
+
+  // 从数据库查询Id为storeId是否存在
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId: userId,
+    }
+  });
+
+  // 判读数据库中是否存在
+  if(!store) {
+    redirect("/");
+  };
+
+  // 渲染layout及子组件页面
+  return (
+    <>
+      <div>This will be a NavBar</div>
+      {/* 渲染子组件 */}
+      {children}
+    </>
+  );
+}
---
+++ ecommerce-admin/app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+
+export default async function SetupLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const {userId} = auth();
+  if(!userId) {
+    redirect("/sign-in");
+  }
+
+  // 从DB查询store信息
+  const store = await prismadb.store.findFirst({
+    where: {
+      userId: userId,
+    }
+  });
+  if(store) {
+    redirect(`/${store.id}`);
+  };
+
+  return (
+    <>
+      {children}
+    </>
+  );
+};
