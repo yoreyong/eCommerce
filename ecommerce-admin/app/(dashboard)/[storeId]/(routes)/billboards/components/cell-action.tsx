7454feb2f94fc8d61c5d93065bd3a045e7d25835
YORE <yoreyongwang@gmail.com>
2023-08-24 23:09:58 -0500

    [admin]feat: 增加data-table

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export const CellAction = () => {
+  return (
+    <div>Action</div>
+  );
+};
--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,19 +6,33 @@ import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 
-export const BillboardClient = () => {
+import { BillBoardColumn, columns } from "./columns";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+
+interface BillboardClientProps {
+  data: BillBoardColumn[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+  data
+}) => {
   const params = useParams();
   const router = useRouter();
 
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title="Billboards(0)"  description="Manage billboards for your store"/>
+        <Heading title={`Billboards (${data.length})`}  description="Manage billboards for your store"/>
         <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
           <Plus className="mr-2 h-4 w-4" />
           Add New
         </Button>
       </div>
+
+      <Separator />
+
+      <DataTable searchKey="label" columns={columns} data={data}/>
     </>
   );
 };
